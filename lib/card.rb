73b7fa99266bb79676137fe5caf64347854f7b75
Stacy <lundquist.stacy@gmail.com>
2020-09-28 21:09:31 -0700

    added ArgumentErrors, case and test for to_s

--- lib/card.rb
+++ lib/card.rb
@@ -6,13 +6,28 @@ class Card
 
   def initialize(value, suit)
     @value = value
+    raise ArgumentError, "That isn't a valid card." if value > 13 || value < 1
+
     @suit = suit
-    
+    valid_suit = [:hearts, :spades, :clubs, :diamonds]
+    raise ArgumentError, "That isn't a valid suit" unless valid_suit.include?(suit)
 
   end
 
   def to_s
-    return "#{value} of #{suit.to_s}"
+    case
+    when value < 11 && value != 1
+      return "#{value} of #{suit.to_s.capitalize}"
+    when value == 1
+      return "Ace of #{suit.to_s.capitalize}"
+    when value == 11
+      return "Jack of #{suit.to_s.capitalize}"
+    when value == 12
+      return "Queen of #{suit.to_s.capitalize}"
+    when value == 13
+      return "King of #{suit.to_s.capitalize}"
+    end
   end
+
 
 end
--- test/card_test.rb
+++ test/card_test.rb
@@ -1,9 +1,9 @@
 require_relative 'test_helper'
 
 describe Card do
-  describe "You can create a Card instance" do
+  describe 'You can create a Card instance' do
 
-    it "Can be created" do
+    it 'Can be created' do
       [:hearts, :spades, :clubs, :diamonds].each do |suit|
         (1..13).each do |value|
           card = Card.new(value, suit)
@@ -12,7 +12,7 @@ describe Card do
       end
     end
 
-    it "Raises an ArgumentError for invalid parameters" do
+    it 'Raises an ArgumentError for invalid parameters' do
       # To raise an ArgumentError in card.rb
       # raise ArgumentError.new
       expect {Card.new(0, :diamonds)}.must_raise ArgumentError
@@ -21,35 +21,38 @@ describe Card do
     end
   end
 
-  describe "Testing to_s" do
+  describe 'Testing to_s' do
 
-    it "to_s returns a readable String value logically for values 2-10" do
-      # Test to ensure that to_s works for cards values 2-10
-      # for example:  "2 of diamonds"
+    it 'to_s returns a readable String value logically for values 2-10' do
+      card = Card.new(2, :diamonds)
+      card2 = Card.new(10, :spades)
+      card3 = Card.new(5, :hearts)
+
+      expect(card.to_s).must_equal '2 of Diamonds'
+      expect(card2.to_s).must_equal '10 of Spades'
+      expect(card3.to_s).must_equal '5 of Hearts'
     end
 
-    it "to_s returns a readable String value for Ace, Jack, Queen, King" do
-      # Test to ensure that to_s works for cards values 1, and 11-13
-      # For example: "Queen of hearts"
-      # The current implementation of to_s does not address this feature
-      # Write the test, see it fail, then modify to_s to make it pass!
-      # (Consider writing a helper method!)
-      #
-      # For reference:
-      #  1: Ace
-      #  11: Jack
-      #  12: Queen
-      #  13: King
+    it 'to_s returns a readable String value for Ace, Jack, Queen, King' do
+      card = Card.new(1, :diamonds)
+      card2 = Card.new(11, :spades)
+      card3 = Card.new(12, :hearts)
+      card4 = Card.new(13, :clubs)
+
+      expect(card.to_s).must_equal 'Ace of Diamonds'
+      expect(card2.to_s).must_equal 'Jack of Spades'
+      expect(card3.to_s).must_equal 'Queen of Hearts'
+      expect(card4.to_s).must_equal 'King of Clubs'
     end
   end
 
-  describe "Reader methods" do
+  describe 'Reader methods' do
 
-    it "Can retrieve the value of the card using a `.value`." do
+    it 'Can retrieve the value of the card using a `.value`.' do
       # ensure that `.value works as expected`
     end
 
-    it "Can retrieve the value of the card using a `.suit`." do
+    it 'Can retrieve the value of the card using a `.suit`.' do
       # ensure that `.suit works as expected returning the symbol of the suit`
 
     end
